5bd602b7672e0f2a3042e01273ef35005d5e62a9
bhuwanjungthapa <bhuwanjung115@gmail.com>
2023-06-17 16:48:47 +0545

    rule added

--- src/components/common/navbar.jsx
+++ src/components/common/navbar.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useContext } from "react";
 import UserContext from "./usercontext";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Navbar = ({ username, onLogout, onSearch, searchProp }) => {
   const { user, setUser } = useContext(UserContext);
   const navigate = useNavigate();
+  const location = useLocation();
 
   const [search, setSearch] = useState(""); // Local state to manage the search input
   useEffect(() => {
@@ -29,6 +30,15 @@ const Navbar = ({ username, onLogout, onSearch, searchProp }) => {
         <div className="flex items-center">
           <span className="font-bold">Hello, {user?.username}</span>
         </div>
+        {location.pathname === "/message" && (
+          <button
+            className="mx-4 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+            onClick={() => navigate("/dashboard")}
+          >
+            Dashboard
+          </button>
+        )}
         <div className="w-full sm:w-auto mt-4 sm:mt-0 flex items-center">
           <input
             type="text"
@@ -36,20 +46,12 @@ const Navbar = ({ username, onLogout, onSearch, searchProp }) => {
             placeholder="Search..."
             className="px-3 py-2 text-black bg-white rounded"
           />
+
           <button
             onClick={handleLogoutClick}
             className="ml-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
           >
             Logout
-          </button>
-        </div>
-        <div className="w-full mt-4 sm:hidden">
-          <button
-            className="w-full bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
-            onClick={() => navigate("/dashboard")}
-          >
-            Dashboard
           </button>
         </div>
       </div>
